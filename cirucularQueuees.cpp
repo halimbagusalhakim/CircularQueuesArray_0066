6d0cbdcf66218d3f8b6c5b34f17664b92bbccc69
halimbagusalhakim <halsat132@gmail.com>
2024-05-31 14:53:43 +0700

    menambahkan conditional else

--- cirucularQueuees.cpp
+++ cirucularQueuees.cpp
@@ -50,7 +50,12 @@ public:
         if (FRONT == REAR) {
             FRONT = -1;
             REAR = -1;
-
+        }
+        else {
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
         }
 
       
